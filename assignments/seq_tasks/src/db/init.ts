255ad09e3b065c6ad98f9bdcf16ff10b0b66cf07
newsnick <atlcontact8@gmail.com>
2023-07-21 15:08:35 +0200

    setup typescrip orm environment

---
+++ assignments/seq_tasks/src/db/init.ts
@@ -0,0 +1,16 @@
+import {
+  Recipe,
+  RecipeTags,
+  Tag,
+  Review,
+  Ingredient,
+  RecipeIngredients,
+} from './models'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const dbInit = () => {
+  Ingredient.sync({ alter: isDev })
+}
+
+export default dbInit
